7a7e2d24b28102bfc612d13a2dfae94361054a94
Samir M R <samir@Samirs-MacBook-Pro.local>
2020-03-01 14:51:06 +0100

    Version 1.0  master git

---
+++ src/components/header/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+
+const Header = () =>{
+    return(
+        <div className = "container">
+            <div className = "row">
+                <div className = "col-12">
+                    <nav>
+                        <h1>From header</h1>
+                    </nav>
+                    
+                </div>
+            </div>
+        </div>
+        
+    )
+}
+
+
+export default Header;

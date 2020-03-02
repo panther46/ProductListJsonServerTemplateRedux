77b072f2377f5c2d46028548c043c8907780530d
Samir M R <samir@Samirs-MacBook-Pro.local>
2020-03-02 20:25:39 +0100

    Setting components products, edit and add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import './App.scss';
 import Header from './components/header/index';
+import Products from './components/products/index';
+import NewProduct from './components/newProduct/index';
+import EditProduct from './components/editProduct/index';
+
+// Importing Router
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Router>
+
+        <Header/>
+        <div className= "container">
+          <Switch>
+            <Route exact path = "/" component = {Products} />
+            <Route exact path = "/products/new" component = {NewProduct} />
+            <Route excct path = "/products/edit" component = {EditProduct} />
+          </Switch>
+        </div>
+
+      </Router>
+      
 
       
     </div>
---
+++ src/components/newProduct/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+function NewProduct(){
+    return(
+        <h1>From New Product</h1>
+    )
+}
+
+export default NewProduct;

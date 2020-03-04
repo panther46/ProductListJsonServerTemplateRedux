ec9b54b423b3e5098eb747d0707e8de274239bec
Samir M R <samir@Samirs-MacBook-Pro.local>
2020-03-04 19:32:07 +0100

    Building main table, adding form in Edit Product

--- src/components/editProduct/index.js
+++ src/components/editProduct/index.js
@@ -4,7 +4,44 @@ import React from 'react';
 
 function EditProduct(){
     return(
-        <h1>From edit product</h1>
+        <div className = "row justify-content-center">
+            <div className = "col-md-8">
+                <div className = "card">
+                    <div className = "card-body">
+                        <h2 className = "text-center mb-4 font-weight-bold">
+                            EditProduct
+                        </h2>
+                        <form>
+                            <div className = "form-group">
+                                <label>Product Name</label>
+                                <input
+                                type= "text"
+                                className = "form-control"
+                                placeholder = "Product Name"
+                                name = "name"
+                                />
+                            </div>
+                            <div className = "form-group">
+                                <label>Product Price</label>
+                                <input
+                                type= "number"
+                                className = "form-control"
+                                placeholder = "Product Price"
+                                name = "price"
+                                />
+                            </div>
+                            <button
+                            type = "submit"
+                            className = "btn btn-primary font-weight-bold text-uppercase d-block w-100"
+                                >Save Changes
+                            </button>
+                        </form>
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
     );
 }
 
--- src/components/products/index.js
+++ src/components/products/index.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 
 
 function Products(){
     return(
-        <p>From products main list</p>
+        <Fragment>
+            <h2 className = "text-center my-5">Product List</h2>
+
+            <table className = "table table-striped">
+                <thead className = " bg-primary table-dark">
+                    <tr>
+                        <th scope = "col">Name</th>
+                        <th scope = "col">Price</th>
+                        <th scope = "col">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                </tbody>
+            </table>
+        </Fragment>
+        
     )
 }
 
